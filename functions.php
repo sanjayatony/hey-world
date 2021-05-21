aede8f80e6ed572f9eee79e90f379d509d35d9e3
Tony S <sanjayatony@gmail.com>
2021-05-22 00:01:13 +0700

    remove unused

--- functions.php
+++ functions.php
@@ -71,18 +71,6 @@ if ( ! function_exists( 'hey_world_setup' ) ) :
 			)
 		);
 
-		// Set up the WordPress core custom background feature.
-		add_theme_support(
-			'custom-background',
-			apply_filters(
-				'hey_world_custom_background_args',
-				array(
-					'default-color' => 'ffffff',
-					'default-image' => '',
-				)
-			)
-		);
-
 		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
@@ -145,18 +133,8 @@ function hey_world_scripts() {
 
 	wp_enqueue_style( 'hey-world-tw', get_template_directory_uri() . '/app.css', array(), _S_VERSION );
 
-	wp_enqueue_script( 'hey-world-navigation', get_template_directory_uri() . '/js/navigation.js', array(), _S_VERSION, true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
 }
 add_action( 'wp_enqueue_scripts', 'hey_world_scripts' );
-
-/**
- * Implement the Custom Header feature.
- */
-require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
