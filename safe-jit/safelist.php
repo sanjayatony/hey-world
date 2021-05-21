73149f453aa5f30e75f1a15d8a257ae18f8c75f6
Tony S <sanjayatony@gmail.com>
2021-05-21 23:58:26 +0700

    add safelitst for JIT

---
+++ safe-jit/safelist.php
@@ -0,0 +1,5 @@
+<nav class="navigation posts-navigation" role="navigation" aria-label="Posts">
+    <h2 class="screen-reader-text">Posts navigation</h2>
+	<div class="nav-links"><div class="nav-previous"><a href="https://tony.sanjaya.info/page/2/">Older posts</a></div></div>
+    <div class="nav-links"><div class="nav-next"><a href="https://tony.sanjaya.info/page/2/">Older posts</a></div></div>
+</nav>
