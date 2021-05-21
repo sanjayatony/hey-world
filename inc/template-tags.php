12af6ce9a3146921099c25d57a6027c2ea6bd653
Tony S <sanjayatony@gmail.com>
2021-05-21 23:58:56 +0700

    remove post on

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -27,7 +27,7 @@ if ( ! function_exists( 'hey_world_posted_on' ) ) :
 
 		$posted_on = sprintf(
 			/* translators: %s: post date. */
-			esc_html_x( 'Posted on %s', 'post date', 'hey-world' ),
+			esc_html_x( '%s', 'post date', 'hey-world' ),
 			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
 		);
 
